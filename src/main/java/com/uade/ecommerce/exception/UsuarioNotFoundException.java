0c11e82b7fed5a37036f8f2b9cf0566b70200266
carolinabarrionuevo <carolinaabarrionuevo@gmail.com>
2026-04-10 16:51:49 -0300

    Update UsuarioNotFoundException.java

--- src/main/java/com/uade/ecommerce/exception/UsuarioNotFoundException.java
+++ src/main/java/com/uade/ecommerce/exception/UsuarioNotFoundException.java
@@ -1,12 +1,7 @@
 package com.uade.ecommerce.exception;
 
-<<<<<<< Updated upstream
-public class UsuarioNotFoundException {
-    
-=======
 public class UsuarioNotFoundException extends RuntimeException {
     public UsuarioNotFoundException(String mensaje) {
         super(mensaje);
     }
->>>>>>> Stashed changes
 }
