5686e45459ad0eca2544623f4f9779cf109e42ac
sbevitore <sbevitore@uade.edu.ar>
2026-04-10 13:07:37 -0300

    add @RestController & @RequestMapping

--- src/main/java/com/uade/ecommerce/controller/CategoriaController.java
+++ src/main/java/com/uade/ecommerce/controller/CategoriaController.java
@@ -8,10 +8,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.uade.ecommerce.model.Categoria;
 import com.uade.ecommerce.service.CategoriaService;
 
+@RestController
+@RequestMapping("/categorias")
 public class CategoriaController {
 
     @Autowired
--- src/main/java/com/uade/ecommerce/controller/PedidoController.java
+++ src/main/java/com/uade/ecommerce/controller/PedidoController.java
@@ -7,10 +7,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.uade.ecommerce.model.Pedido;
 import com.uade.ecommerce.service.PedidoService;
 
+@RestController
+@RequestMapping("/pedidos")
 public class PedidoController {
     private PedidoService pedidoServ;
 
