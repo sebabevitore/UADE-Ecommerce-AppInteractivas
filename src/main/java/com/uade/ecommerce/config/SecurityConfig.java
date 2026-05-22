a2a9d8e7b927322e995e8f4b6bc9069b64c55285
sbevitore <sbevitore@uade.edu.ar>
2026-05-22 12:11:15 -0300

    add cors

--- src/main/java/com/uade/ecommerce/config/SecurityConfig.java
+++ src/main/java/com/uade/ecommerce/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.uade.ecommerce.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -14,8 +16,10 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import com.uade.ecommerce.model.Role;
 import com.uade.ecommerce.repository.UsuarioRepository;
 import com.uade.ecommerce.security.JwtFilter;
 
@@ -98,6 +102,7 @@ public class SecurityConfig {
     // return http.build();
 
     http
+        .cors(cors -> cors.configurationSource(corsConfigurationSource()))
         .csrf(csrf -> csrf.disable())
         
         .authorizeHttpRequests(auth -> auth
@@ -145,4 +150,16 @@ public class SecurityConfig {
 
     return http.build();
   }
+
+      @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 }
